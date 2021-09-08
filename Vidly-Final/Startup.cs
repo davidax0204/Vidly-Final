de9e9bea3902c7b19d35f7c199b1ad7083c69843
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2021-09-08 15:34:41 +0300

    Add project files.

---
+++ Vidly-Final/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Final.Startup))]
+namespace Vidly_Final
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
